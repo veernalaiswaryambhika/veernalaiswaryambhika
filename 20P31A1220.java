99aa07c97f6709713b4a4a112c935065e2d419d2
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-12-16 11:34:07 +0530

    Create 20P31A1220.java

---
+++ 20P31A1220.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sample
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int sc=s.nextInt();
+    int sc=s.nextInt();
+    System.out.println(a+b);
+    }
+    }
